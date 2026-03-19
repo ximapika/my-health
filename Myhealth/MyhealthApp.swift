2840ccb985718dd9c4049573ff137ebbc515d8a0
zhangyan <2697164036@qq.com>
2026-03-19 23:53:59 +0800

    Initial Commit

---
+++ Myhealth/MyhealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyhealthApp.swift
+//  Myhealth
+//
+//  Created by ximapika on 2026/3/19.
+//
+
+import SwiftUI
+
+@main
+struct MyhealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
